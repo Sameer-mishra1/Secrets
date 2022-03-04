d2d71f795b9cc4f7ad7c18c15b0eba27c89149ca
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2022-03-04 09:56:17 +0530

    Added hashing

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const { stringify } = require("querystring");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,12 +24,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-
 // Creating a new Mongoose model
 const User = new mongoose.model("User", userSchema);
-
-
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -47,7 +42,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (err) {
@@ -60,7 +55,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
@@ -73,9 +68,7 @@ app.post("/login", function(req, res) {
             };
         };
     });
-
 });
-
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
